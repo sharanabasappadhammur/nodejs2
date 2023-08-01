ca67add62841667be2e1d68cc756967e8f65e389
sharanabasappadhammur <108877043+sharanabasappadhammur@users.noreply.github.com>
2023-08-01 12:11:57 +0530

    (1)

---
+++ index.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import tough from "tough-cookie"
+import fetch from "node-fetch";
+import express, { response } from "express"
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+import cors from 'cors'
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, async () => {
+    try {
+        console.log("connected to server")
+    }
+    catch (error) {
+        console.log(error.message)
+    }
+})
+
+
+// Error: getaddrinfo ENOTFOUND tincaphe.com, what does this error means
